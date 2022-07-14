785d020658d6065be6ec6e3564360ba2e8d6f5e6
Vikram Kumar Chellamuthu <vikram.nsr@gmail.com>
2022-07-14 18:29:41 -0400

    class modification

--- F2.java
+++ F2.java
@@ -4,25 +4,6 @@ import java.util.InputMismatchException;
 
 public class F2 {
 
-	final static double pi = 3.14159;
-
-	static double tanXPolynomialOperations(double x) {
-		if (x == 0 || x == 180)
-			return 0.0;
-		else if (x == 45)
-			return 1.0;
-		else if (x < 22.5) {
-			x = x * (pi / 180);
-			double cube = x * x * x;
-			double square = x * x;
-			return x + ((cube) / 3) + ((2 * (square * cube)) / 15) + ((17 * (cube * cube * x)) / 315);
-		} else if (x >= 22.5 && x < 45)
-			return (2 * (tanXPolynomialOperations(x / 2))) / (1 - (tanXPolynomialOperations(x / 2) * tanXPolynomialOperations(x / 2)));
-		else if (x >= 45 && x < 90)
-			return 1 / tanXPolynomialOperations(90 - x);
-		else
-			return -tanXPolynomialOperations(180 - x);
-	}
 
 	public static double reducesAnguleTaxX(double x) {
 		x = x % 360;
